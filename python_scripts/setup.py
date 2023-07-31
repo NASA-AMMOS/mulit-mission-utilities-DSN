d1f6f80b69a8b3ccab3aa7e9f533c56946792e9e
Joseph Kaminsky <joseph.j.kaminsky@nasa.gov>
2023-07-31 16:16:38 -0400

    Setup scripts

---
+++ python_scripts/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='libaerie',
+    version='2023.07.31',
+    install_requires=[
+        'requests',
+        'importlib-metadata; python_version == "3.8"',
+    ],
+)
