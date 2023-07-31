c5831c4d6b7f529c54175559a0beb5258c30429e
Joseph Kaminsky <joseph.j.kaminsky@nasa.gov>
2023-07-31 15:59:23 -0400

    Parser regression tests

---
+++ python_scripts/tests/products/test_product_parser.py
@@ -0,0 +1,32 @@
+import io
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, os.path.pardir)))
+from libaerie.products.product_parser import DsnViewPeriodPredLegacyDecoder, DsnStationAllocationFileDecoder, DsnViewPeriodPredLegacyEncoder,DsnStationAllocationFileEncoder, GqlInterface
+
+
+def test_saf_decoder_encoder(saf_content):
+
+    saf_file = DsnStationAllocationFileDecoder(io.StringIO(saf_content))
+    saf_header = saf_file.read_header()
+    saf_iter = saf_file.parse()
+
+    saf_out = io.StringIO()
+    saf_encoder = DsnStationAllocationFileEncoder(saf_out, saf_header)
+    saf_encoder.cast(saf_iter)
+
+    assert(saf_out.getvalue() == saf_content)
+
+
+def test_vp_decoder_encoder(vp_content):
+
+    vp_file = DsnViewPeriodPredLegacyDecoder(io.StringIO(vp_content))
+    vp_header = vp_file.read_header()
+    vp_iter = vp_file.parse()
+
+    vp_out = io.StringIO()
+    vp_encoder = DsnViewPeriodPredLegacyEncoder(vp_out, vp_header)
+    vp_encoder.cast(vp_iter)
+
+    assert(vp_out.getvalue() == vp_content)
