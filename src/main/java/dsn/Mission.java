5052644925069df1097a5e442f69be02baee6286
Meghana Kumar <meghana.n.kumar@jpl.nasa.gov>
2023-08-19 14:06:25 -0700

    Az El Per Station

--- src/main/java/dsn/Mission.java
+++ src/main/java/dsn/Mission.java
@@ -1,11 +1,15 @@
 package dsn;
 
-import gov.nasa.jpl.aerie.contrib.models.Register;
-import gov.nasa.jpl.aerie.contrib.serialization.mappers.EnumValueMapper;
+import dsn.models.Az_El_Per_Station;
+import gov.nasa.jpl.aerie.contrib.serialization.mappers.DoubleValueMapper;
+import gov.nasa.jpl.aerie.contrib.serialization.mappers.StringValueMapper;
 import gov.nasa.jpl.aerie.merlin.framework.Registrar;
 
 public final class Mission {
 
   public Mission(final Registrar registrar, final Configuration config) {
+    Az_El_Per_Station az_el_DSS_13 = new Az_El_Per_Station("/az_el_DSS-13.txt");
+    registrar.discrete("/DSS_13_Azimuth", az_el_DSS_13.azimuth, new DoubleValueMapper());
+    registrar.discrete("/my_path", az_el_DSS_13.my_path, new StringValueMapper());
   }
 }
