c17c1a9b60507a360bb47117e83fb370b27d107a
Meghana Kumar <meghana.n.kumar@jpl.nasa.gov>
2023-08-19 17:27:10 -0700

    Pushes view periods to UI

--- python_scripts/libaerie/spice_calcs/az_el.py
+++ python_scripts/libaerie/spice_calcs/az_el.py
@@ -2,6 +2,8 @@ import spiceypy
 import spiceypy.utils.support_types as stypes
 from datetime import datetime, timedelta
 import numpy as np
+import json
+import requests
 
 spiceypy.furnsh("erotat.tm")
 
@@ -102,18 +104,23 @@ def el_az_driver(config):
 		results = el_az_computer(this_step.strftime(time_format), config.chosen_dss, str(config.spacecraft), config.dss_data, elapsed_seconds)
 		this_step += timedelta(seconds = config.step)
 	return
+
+
+def view_pr_driver(config):
+	start = datetime.strptime(config.start, config.time_format)
+	view_pr(start, config.dss_data)
 	
-def view_pr():
-	MAXIVL = 1000
+def view_pr(plan_start, stations):
+	MAXIVL = 10
 	MAXWIN = 2 * MAXIVL
-	srfpt  = 'DSS-14'
-	obsfrm = 'DSS-14_TOPO'
 	target =  '-159'
 	abcorr = 'CN+S'
-	start  = '2028 MAY 2 TDB'
-	stop   = '2028 MAY 6 TDB'
+	start  = '2028 MAY 2'
+	stop   = '2028 MAY 6'
 	elvlim =  6.0
 	revlim = spiceypy.rpd() * elvlim
+	TDBFMT = 'YYYY MM DD HR:MN:SC.###'
+	date_format = '%Y %m %d %H:%M:%S.%f'
 	crdsys = 'LATITUDINAL'
 	coord  = 'LATITUDE'
 	relate = '>'
@@ -123,24 +130,108 @@ def view_pr():
 	etend = spiceypy.str2et( stop  )
 
 	cnfine = stypes.SPICEDOUBLE_CELL(2)
-	spiceypy.wninsd( etbeg, etend, cnfine )
-	riswin = stypes.SPICEDOUBLE_CELL( MAXWIN )
-	spiceypy.gfposc( target, obsfrm, abcorr, srfpt,
-	crdsys, coord,  relate, revlim,
-	adjust, stepsz, MAXIVL, cnfine, riswin )
+	view_pr_acts = []
 
-	result = spiceypy.gfposc(target, obsfrm, abcorr, srfpt,crdsys, coord,  
-		relate, revlim,adjust, stepsz, MAXIVL, cnfine, riswin )
-	print(riswin)
+	for antenna in stations:
+
+		srfpt  = antenna
+		obsfrm = antenna+'_TOPO'
+
+		spiceypy.wninsd( etbeg, etend, cnfine )
+		riswin = stypes.SPICEDOUBLE_CELL( MAXWIN )
+		spiceypy.gfposc( target, obsfrm, abcorr, srfpt,
+		crdsys, coord,  relate, revlim,
+		adjust, stepsz, MAXIVL, cnfine, riswin )
+
+		result = spiceypy.gfposc(target, obsfrm, abcorr, srfpt,crdsys, coord,  
+			relate, revlim,adjust, stepsz, MAXIVL, cnfine, riswin )
+
+		winsiz = spiceypy.wncard(riswin)
+
+		if winsiz == 0:
+			print( 'No events were found.' )
+		else:
+			print('Visibility times of {0:s} as seen from {1:s}:\n'.format(target,srfpt))
+
+		for i in range(winsiz):
+			# Fetch the start and stop times of
+			# the ith interval from the search result
+			# window riswin.
+			[intbeg, intend] = spiceypy.wnfetd(riswin, i)
+
+			#
+			# Convert the rise time to a TDB calendar string.
+			#
+			timestr1 = spiceypy.timout(intbeg, TDBFMT)
+			timestr2 = spiceypy.timout(intend, TDBFMT)
+			activity_start = datetime.strptime(timestr1, date_format)
+			activity_end = datetime.strptime(timestr2, date_format)
+
+			elapsed_seconds_since_activity_start = timedelta.total_seconds(activity_start-plan_start)
+	
+
+			activity_duration = timedelta.total_seconds(activity_end-activity_start)
+			activity_duration = activity_duration*1000000;
+			activity_duration = int(activity_duration)
+			activity_name = antenna + " View Period"
+
+			print(activity_duration)
+
+			print(f"start: {timestr1}")
+			print(f"end: {timestr2}\n")
+			activity_data = {
+				'arguments' : {'NAIF_spacecraft_ID':'-159', 'station_identifier':antenna, 'Duration': activity_duration},
+				'plan_id' : 114,
+				'name' : activity_name,
+				'start_offset': elapsed_seconds_since_activity_start,
+				'type': 'DSN_View_Period_Duration'
+			}
+			view_pr_acts.append(activity_data)
+	
+	api_url = 'http://localhost:8080/v1/graphql' # https://aerie-dev.jpl.nasa.gov:8080/v1/graphql
+	query = '''mutation InsertActivities($activities: [activity_directive_insert_input!]!) {insert_activity_directive(objects: $activities) {returning {id name } } } '''
+	
+	activity_data = {
+	'arguments' : {'spacecraft_ID':-159, 'station_identifier':int(antenna[4:]), 'duration': activity_duration},
+	'plan_id' : 114,
+	'name' : activity_name,
+	'start_offset': '319874.782',
+	'type': 'DSN_View_Period_Duration'
+	}	
+
+	view_pr_acts = [activity_data]
+
+	response = requests.post(
+		url=api_url,
+		json={
+		'query': query,
+		 'variables': { "activities": view_pr_acts },
+		},
+		verify=False
+		)
+
+	print("SENT:")
+	print(view_pr_acts)
+
+	print("RESPONSE")
+	print(json.dumps(response.json(), indent=2))
+
 
 time_format = "%Y-%m-%dT%H:%M:%S.%f"
-start = "2025-08-18T00:00:00.00"
-end = "2026-08-18T00:00:00.00"
-step_size = 3600
+start = "2028-05-02T00:00:00.00"
+end = "2028-05-06T00:00:00.00"
+step_size = 60
 chosen_dss = ['DSS-13','DSS-14', 'DSS-25', 'DSS-26', 'DSS-34', 'DSS-65']
 spacecraft = -159 	# -159 is Clipper
 
 config = python_dss_configuration(time_format, start, end, step_size, chosen_dss, spacecraft)
-el_az_driver(config)
-config.print_az_el_data()
-#view_pr()
+
+# FOR ELEVATION AND AZIMUTH
+#el_az_driver(config)
+#config.print_az_el_data()
+
+# FOR VIEW PERIODS
+view_pr_driver(config)
+
+
+
